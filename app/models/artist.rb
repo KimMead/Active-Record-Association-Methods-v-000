7ac96922695c306d12e7aa3340c8259c275a5240
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-10-23 15:05:03 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,5 +14,8 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
+    genre = self.songs.collect do |song| 
+      song.genre 
   end
+  
 end
